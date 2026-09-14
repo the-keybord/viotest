bd117eb086ffaeb7f2121fc68eb09215673e5b43
keybord <andri40567@gmail.com>
2026-09-14 14:40:42 +0300

    feat: implement URL shortening API endpoint

--- api/shorten.php
+++ api/shorten.php
@@ -36,7 +36,7 @@ if (empty($url)) {
 
 // Auto-prefix protocol if missing (e.g. google.com -> https://google.com)
 if (!preg_match('~^(?:f|ht)tps?://~i', $url)) {
-    $url = 'https://' . $url;
+    $url = $url;
 }
 
 // Validate URL structure
